5524effb3978b07faedf67e839243c3d843951cf
Saba Habtamu <sabahabtamuu@gmail.com>
2025-10-27 14:39:09 +0300

    Update Header component
    Added a background image to the header component.

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -2,7 +2,7 @@ import './Header.css'
 
 const Header = () => {
   return (
-    <div className='header'>
+    <div className='header' style={{backgroundImage: `url(${headerImage})`}}>
       <div className="header-content">
         <h2>Order your favourite food here</h2>
         <p>Choose from a diverse menu featuring a delectable array of dishes craffed with the finest ingredients and culinary expertise. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.</p>
@@ -13,3 +13,4 @@ const Header = () => {
 }
 
 export default Header
+
